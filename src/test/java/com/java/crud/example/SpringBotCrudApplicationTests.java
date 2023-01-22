0cdc02a4d60e1b8afaf460a6072519a93ad89562
Albab <alb.ab3714@gmail.com>
2023-01-23 00:04:27 +0530

    initial commit

---
+++ src/test/java/com/java/crud/example/SpringBotCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
